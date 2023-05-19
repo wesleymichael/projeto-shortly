dc20e617a8cd5b8b0d8c892bc28685de25c7a3f6
wesleymichael <wesleymichaelpereira@hotmail.com>
2023-05-19 16:24:34 -0300

    feat: added functionality to redirect URL

--- src/controllers/url.controller.js
+++ src/controllers/url.controller.js
@@ -26,16 +26,32 @@ export async function getUrlById(req, res){
     if(isNaN(id)) return res.sendStatus(400);
 
     try {
-        const url = await db.query(`
+        const urlData = await db.query(`
             SELECT "urls".id, "urls"."shortUrl", "urls"."url" 
             FROM "urls"
             WHERE id = $1;
         `, [id]);
 
-        if(url.rowCount === 0) return res.sendStatus(404);
+        if(urlData.rowCount === 0) return res.sendStatus(404);
 
-        return res.status(200).send(url.rows[0]);
+        return res.status(200).send(urlData.rows[0]);
     } catch (error) {
         return res.status(500).send(error.messsage)
+    }
+}
+
+export async function redirectToUrl(req, res){
+    const {shortUrl} = req.params;
+    
+    try {
+        const urlData = await db.query(`SELECT * FROM urls WHERE "shortUrl" = $1;`, [shortUrl]);
+
+        if(urlData.rowCount === 0) return res.sendStatus(404);
+
+        const absoluteUrl = urlData.rows[0].url.startsWith('http') ? urlData.rows[0].url : `http://${urlData.rows[0].url}`;
+        return res.redirect(absoluteUrl);
+
+    } catch (error) {
+        return res.status(500).send(error.message)
     }
 }
--- src/routers/url.router.js
+++ src/routers/url.router.js
@@ -2,11 +2,12 @@ import { Router } from "express";
 import { validadeSchema } from "../middlewares/validateSchema.middleware.js";
 import { urlSchema } from "../schemas/url.schemas.js";
 import { authValidation } from "../middlewares/authValidation.middlewares.js";
-import { getUrlById, shortenUrl } from "../controllers/url.controller.js";
+import { getUrlById, redirectToUrl, shortenUrl } from "../controllers/url.controller.js";
 
 const urlRouter = Router();
 
 urlRouter.post("/urls/shorten", validadeSchema(urlSchema), authValidation, shortenUrl);
 urlRouter.get("/urls/:id", getUrlById);
+urlRouter.get("/urls/open/:shortUrl", redirectToUrl);
 
 export default urlRouter;
